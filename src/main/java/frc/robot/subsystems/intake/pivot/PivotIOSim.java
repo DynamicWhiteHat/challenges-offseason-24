38afaf30355ca85ed2bb8dec1b01e65f8a2d0e28
DynamicWhiteHat <aahil.syed.usa@gmail.com>
2024-09-14 15:44:44 -0400

    Shirhari's Fat

--- src/main/java/frc/robot/subsystems/intake/pivot/PivotIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/pivot/PivotIOSim.java
@@ -1,16 +1,29 @@
 package frc.robot.subsystems.intake.pivot;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import frc.robot.Constants.IntakeConstants;
 
 public class PivotIOSim implements PivotIO {
   // For instructions on how to implement this class, refer to the README.md file
 
   private SingleJointedArmSim m_sim;
   // define more members here as necessary
+  private double motorvoltage;
 
   public PivotIOSim() {
     // TODO: Implement this constructor
+    m_sim =
+        new SingleJointedArmSim(
+            DCMotor.getNEO(1),
+            IntakeConstants.kPivotGearing,
+            IntakeConstants.kPivotJKgMetersSquared,
+            IntakeConstants.kPivotLength,
+            IntakeConstants.kPivotMinAngle,
+            IntakeConstants.kPivotMaxAngle,
+            false,
+            Math.toRadians(120));
   }
 
   @Override
@@ -25,23 +38,25 @@ public class PivotIOSim implements PivotIO {
   @Override
   public void setVoltage(double voltage) {
     // TODO: Implement this method
+    m_sim.setInputVoltage(voltage);
+    motorvoltage = voltage;
   }
 
   @Override
   public double getVoltage() {
     // TODO: Implement this method
-    return 0.0;
+    return motorvoltage;
   }
 
   @Override
   public double getVelocityRadPerSec() {
     // TODO: Implement this method
-    return 0.0;
+    return m_sim.getVelocityRadPerSec();
   }
 
   @Override
   public Rotation2d getAngle() {
     // TODO: Implement this method
-    return null;
+    return Rotation2d.fromRadians(m_sim.getAngleRads());
   }
 }
--- src/main/java/frc/robot/subsystems/intake/rollers/RollerIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/rollers/RollerIOSim.java
@@ -1,21 +1,28 @@
 package frc.robot.subsystems.intake.rollers;
 
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import frc.robot.Constants.IntakeConstants;
 
 public class RollerIOSim implements RollerIO {
   // For instructions on how to implement this class, refer to the README.md file
 
   private DCMotorSim m_sim;
   // define more members here as necessary
+  private double truevoltage;
 
   public RollerIOSim() {
     // TODO: Implement this constructor
+    m_sim =
+        new DCMotorSim(
+            DCMotor.getKrakenX60(1),
+            IntakeConstants.kRollerGearing,
+            IntakeConstants.kRollerJKgMetersSquared);
   }
 
   @Override
   public void updateInputs(RollerInputs inputs) {
     m_sim.update(0.02);
-
     inputs.voltage = getVoltage();
     inputs.velocityRadPerSec = getVelocityRadPerSec();
   }
@@ -23,17 +30,19 @@ public class RollerIOSim implements RollerIO {
   @Override
   public void setVoltage(double voltage) {
     // TODO: Implement this method
+    truevoltage = voltage;
+    m_sim.setInputVoltage(voltage);
   }
 
   @Override
   public double getVoltage() {
     // TODO: Implement this method
-    return 0.0;
+    return truevoltage;
   }
 
   @Override
   public double getVelocityRadPerSec() {
     // TODO: Implement this method
-    return 0.0;
+    return m_sim.getAngularVelocityRadPerSec();
   }
 }
